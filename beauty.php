a07271b4dbf63279483c33b587c2c65ea11fdb53
vindi <you@example.com>
2016-01-08 18:19:21 +0700

    add tamvan

---
+++ beauty.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Putri..we..web</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="main">
+<div id="header">
+<img src="images/header.gif" width="951" height="184" />
+</div>
+<div id="menu-atas">
+<li><a href="beranda.php"> Beranda </a></li>
+<li><a href="beauty.php?page=beauty"> Beauty Country's </a></li>
+<li><a href="about.php?page=about"> About Dope N Campus </a></li>
+<li><a href="comment.php?page=comment"> Comment </a></li>
+</div>
+<div id="menu-tengah">
+<div id="bg-menu"><center> Beautiful Country In The World </center>
+</div>
+<div id="content-menu">
+<table cellspacing="6">
+<tr>
+    <td width="437" height="419" style="border:dotted 4px #0000CC"><br/><font size="5" color="#9900CC" face="tahoma"><center> Indonesia </center></font><br/> <img src="images/indonesia.jpg" width="424" height="350" class="gambar"></td>
+    <td width="473" style="border:dotted 4px #0000CC"><br/><font size="5" color="#9900CC" face="tahoma"><center> Italia </center></font><br/> <img src="images/italia.jpg" width="457" height="350" class="gambar"></td>
+</tr>
+<tr>
+    <td colspan="2" style="border:dotted 4px #0000CC"><br/><font size="5" color="#9900CC" face="tahoma"><center> Kepulauan Solomon </center></font><br/><img src="images/solomon.jpg" width="895" height="350" alt="solomon" /></td>
+    </tr>
+<tr>
+    <td style="border:dotted 4px #0000CC"><br/><font size="5" color="#9900CC" face="tahoma"><center> Yunani </center></font><br/>
+      <img src="images/Yunani.jpg" width="421" height="350"  class="gambar"></td>
+	<td style="border:dotted 4px #0000CC"><br/><font size="5" color="#9900CC" face="tahoma"><center> Brazil </center></font><br/><img src="images/brazil.jpg" width="453" height="350"  class="gambar"></td>
+</tr>
+
+ 	
+</tr>
+</table>
+</div>
+</div>
+<div id class="clear">
+</div>
+<div id="footer">
+<img src="images/footer.gif" width="952" />
+</div>
+</div>
+</body>
+</html>
